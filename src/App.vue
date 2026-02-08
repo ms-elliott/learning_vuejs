a237b6bfb297ce12b90c1dcae053457db7d91061
ms-elliott <ms.elliott55@gmail.com>
2026-02-08 23:40:22 +0900

    basic lesson about components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import Section3 from './components/Section3.vue'
-import Section4 from './components/Section4.vue'
-import Section5 from './components/Section5.vue'
+import Section3 from './components/LessonSection3.vue'
+import Section4 from './components/LessonSection4.vue'
+import Section5 from './components/LessonSection5.vue'
+import Section6 from './components/LessonSection6.vue'
 </script>
 <template>
   <Section3 />
   <Section4 />
   <Section5 />
+  <Section6 />
 </template>
---
+++ src/components/LessonSection6.vue
@@ -0,0 +1,22 @@
+// 親子間のコンポーネントでデータを受け渡す // [props]親から子にデータを渡す
+<!-- App.vue 親-->
+<script setup>
+import { ref } from 'vue'
+import ShowCount from './components/ShowCount.vue'
+const count = ref(0)
+</script>
+<template>
+  <ShowCount :foo="count" bar="hello" />
+  // データを渡す為の特別な属性=Props
+</template>
+<!-- ShowCount.vue 子 -->
+<script setup>
+const props = defineProps(['foo', 'bar'])
+console.log(props.foo)
+</script>
+<template>
+  <p>count: {{ foo }}</p>
+  <p>{{ bar }}</p>
+</template>
+<!-- propsは読み取り専用の為、子コンポーネントで値の書き換えはできない
+子コンポーネントでpropsとして定義した値は、属性は継承されない -->
