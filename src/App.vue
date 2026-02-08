b9ea153337d3e3af26b0a723abe08b919e5e9367
ms-elliott <ms.elliott55@gmail.com>
2026-02-09 02:22:43 +0900

    basic lesson for demo app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,65 @@
 <script setup>
-import Section3 from './components/LessonSection3.vue'
-import Section4 from './components/LessonSection4.vue'
-import Section5 from './components/LessonSection5.vue'
-import Section6 from './components/LessonSection6.vue'
+// import Section3 from './components/LessonSection3.vue'
+// import Section4 from './components/LessonSection4.vue'
+// import Section5 from './components/LessonSection5.vue'
+// import Section6 from './components/LessonSection6.vue'
+import { ref } from 'vue'
+import FoodCard from './components/LessonSection7.vue'
+const foods = ref([
+  { name: '寿司', emoji: '🍣' },
+  { name: 'ハンバーガー', emoji: '🍔' },
+  { name: '焼肉', emoji: '🥩' },
+  { name: '麺系', emoji: '🍜' },
+  { name: 'たこ焼き', emoji: '🐙' },
+  { name: 'おでん', emoji: '🍢' },
+  { name: '丼系', emoji: '🍚' },
+  { name: 'オムライス', emoji: '🥚' },
+  { name: 'イタリアン', emoji: '🇮🇹' },
+  { name: '中華料理', emoji: '🇨🇳' },
+  { name: 'とんかつ', emoji: '🐖' },
+  { name: '韓国料理', emoji: '🇰🇷' },
+  { name: 'カレー', emoji: '🍛' },
+  { name: '鍋系', emoji: '🍲' },
+  { name: '居酒屋', emoji: '🏮' },
+  { name: 'ファミレス', emoji: '🍽️' },
+])
+const pickedFoodIndex = ref(0)
+const newFoodIndex = ref(1)
+function pickFood(index) {
+  pickedFoodIndex.value = index
+  newFoodIndex.value++
+}
 </script>
 <template>
-  <Section3 />
+  <!-- <Section3 />
   <Section4 />
   <Section5 />
-  <Section6 />
+  <Section6 /> -->
+  <main>
+    <h1 class="title">今日何食べる?</h1>
+    <FoodCard
+      :name="foods[pickedFoodIndex].name"
+      :emoji="foods[pickedFoodIndex].emoji"
+      @click="pickFood(pickedFoodIndex)"
+    />
+    <template v-if="newFoodIndex < foods.length">
+      <p>YS</p>
+      <FoodCard
+        :name="foods[newFoodIndex].name"
+        :emoji="foods[newFoodIndex].emoji"
+        @click="pickFood(newFoodIndex)"
+      />
+    </template>
+  </main>
 </template>
+<style scoped>
+main {
+  width: 350px;
+  margin: 100px auto;
+  text-align: center;
+}
+
+.title {
+  margin-bottom: 60px;
+}
+</style>
--- src/components/LessonSection6.vue
+++ src/components/LessonSection6.vue
@@ -1,5 +1,5 @@
 // 親子間のコンポーネントでデータを受け渡す // [props]親から子にデータを渡す
-<!-- App.vue 親-->
+<!-- // App.vue 親
 <script setup>
 import { ref } from 'vue'
 import ShowCount from './components/ShowCount.vue'
@@ -9,7 +9,7 @@ const count = ref(0)
   <ShowCount :foo="count" bar="hello" />
   // データを渡す為の特別な属性=Props
 </template>
-<!-- ShowCount.vue 子 -->
+// ShowCount.vue 子
 <script setup>
 const props = defineProps(['foo', 'bar'])
 console.log(props.foo)
@@ -17,6 +17,85 @@ console.log(props.foo)
 <template>
   <p>count: {{ foo }}</p>
   <p>{{ bar }}</p>
-</template>
+</template> -->
 <!-- propsは読み取り専用の為、子コンポーネントで値の書き換えはできない
 子コンポーネントでpropsとして定義した値は、属性は継承されない -->
+
+// バリデーションを利用してpropsに予期しないデータが渡るのを防ぐ
+<!-- // App.vue 親
+<script setup>
+import { ref } from 'vue'
+import ShowCount from './components/ShowCount.vue'
+const count = ref(0)
+</script>
+<template>
+  <ShowCount :foo="count" bar="hello" />
+</template>
+
+// ShowCount.vue 子
+<script setup>
+// definePropsの外で定義された値は使用できない。必ずdefinePropsの中で完結させる必要がある
+defineProps({
+  foo: {
+    // 数値or文字列であればよい場合は、配列で指定。 CustomClassも指定可能。
+    type: [Number, String],
+    // defaultプロパティの値が設定されていない場合は、Undefinedとなる。  (Typeをbooleanにした場合のみ、default値===undefinedがfalseになる)
+    default: 100,
+  },
+  boo: {
+    type: Number,
+    default: function () {
+      // デフォルトの値がオブジェクトor配列の場合は直接指定できない為、関数で記述する
+      return [100, 0]
+    },
+    validator: function (value) {
+      // validator：　戻り値がfalseの場合に警告が出る
+      return value === 0 || value === 1
+    },
+  },
+})
+</script>
+<template>
+  <p>count: {{ foo }}</p>
+</template> -->
+
+// propsの命名規則
+<!-- 子コンポーネントのprops(キャメルケース)を、親コンポーネントで属性として指定する場合はケバブケースで記述することを推奨 -->
+
+// [emit] イベントを発生させ、子ー＞親に通信する // 親 App.vue
+<!-- <script setup>
+import { ref } from 'vue'
+import RessetButton from './components/ReetButton.vue'
+
+// 子コンポーネントの第二引数の受け取り方②メソッドハンドラーで受け取る
+function onReset(value) {
+  count.value = value
+}
+
+const count = ref(0)
+</script>
+<template>
+  <p>count: {{ count }}</p>
+  <button @click="count++">+1</button>
+  // 子コンポーネントの第二引数の受け取り方①$eventで受け取る
+  <ResetButton @reset="count = $event" />
+</template>
+// 子 ResetButton.vue
+<template>
+  // 第二引数にデータを指定することも可能
+  <button @click="$emit('reset', 100)">Reset</button>
+</template> -->
+
+// [defineEmits] 明示的にemitするイベントを示す
+<!-- <script setup>
+const emit = defineEmits(['reset'])
+function emitReset() {
+  emit('reset', 100)
+}
+</script>
+<template>
+  <button @click="emitReset">Reset</button>
+</template> -->
+
+// emitの命名規則
+<!-- props同様に、子コンポーネントのemit(キャメルケース)を、親コンポーネントで属性として指定する場合はケバブケースで記述することを推奨 -->
